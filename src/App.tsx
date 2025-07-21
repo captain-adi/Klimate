81c100a193b2fc1b50a382a34a1312f9ab60d4d9
captain-adi <maniramp554@gmail.com>
2025-07-21 20:16:31 +0530

    fix: test commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,28 @@ import { ThemeProvider } from "./context/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
-  defaultOptions : {
-    queries : {
-      staleTime :5*60*1000 ,
-      gcTime :5*60*1000  ,
-      retry :false ,
-      refetchOnWindowFocus: false
-    }
-  }
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 5 * 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
 });
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="dark">
-      <div className="bg-background-to-br from-background to-muted ">
-        <Header />
-        <main className="container py-12 mx-auto">
-          <Outlet />
-        </main>
-      </div>
-    </ThemeProvider>
- 
-      </QueryClientProvider>
+      <ThemeProvider defaultTheme="dark">
+        <div className="bg-background-to-br from-background to-muted ">
+          <Header />
+          <main className="container py-12 mx-auto">
+            <Outlet />
+          </main>
+        </div>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
